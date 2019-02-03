95f311a7bbc3c6e46b577227cfbf11a22c854886
Andrii Hrynenko <pathliving@gmail.com>
2019-02-03 23:58:25 +0200

    Fix: copy image files

--- config.js
+++ config.js
@@ -31,7 +31,7 @@ let config = {
     "src/fonts/OpenSans.ttf": "fonts/",
     "src/fonts/OpenSans-Semibold.ttf": "fonts/",
     "src/fonts/OpenSans-Bold.ttf": "fonts/",
-    "src/img/demo-avatar-*": "img/",
+    "src/img/*git .{png,svg,jpg,jpeg}": "img/",
     // "src/img/DSC_*": "img/",
     // "src/favicon/*.{png,ico,svg,xml,webmanifest}": "img/favicon",
   },
