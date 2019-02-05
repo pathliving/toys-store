05a621a4c80d052a9de2f652e91c155ae5852508
Andrii Hrynenko <pathliving@gmail.com>
2019-02-06 00:02:07 +0200

    Customize main navigation

--- src/js/script.js
+++ src/js/script.js
@@ -1,8 +1,25 @@
-// const ready = require('./utils/documentReady.js');
+$(document).ready(function () {
+	svg4everybody({});
 
-// ready(function(){
-//   console.log('DOM героически построен!');
-// });
+	let mainSubnavHover = () => {
+		$('.main-subnav__item').hover(
+			function () {
+				let parentList = $(this).closest('.main-subnav__list');
+				if ($(this).children('.main-subnav__list').length) {
+					let caNavHeight = $(this).children('.main-subnav__list').outerHeight();
+					if (parentList.outerHeight() < catNavHeight) {
+						parentList.css('height', catNavHeight);
+					}
+					parentList.css('width', '720');
+				}
+			}, function () {
+				let parentList = $(this).closest('.main-subnav__list');
+				parentList.css('height', 'auto');
+				parentList.css('width', 'auto');
+			}
+		)
+	};
 
-// const $ = require('jquery');
-// $( document ).ready(function() {});
+	mainSubnavHover();
+
+});
