a8365580ecf3086b1940bad17626f3f21a9ad3f8
Andrii Hrynenko <pathliving@gmail.com>
2019-02-05 13:22:55 +0200

    Uncomment sprite-svg block

--- config.js
+++ config.js
@@ -8,7 +8,7 @@ let config = {
     "no-js"
   ],
   "alwaysAddBlocks": [
-    // "sprite-svg",
+    "sprite-svg",
     // "sprite-png",
     // "object-fit-polyfill",
   ],
@@ -31,7 +31,7 @@ let config = {
     "src/fonts/OpenSans.ttf": "fonts/",
     "src/fonts/OpenSans-Semibold.ttf": "fonts/",
     "src/fonts/OpenSans-Bold.ttf": "fonts/",
-    "src/img/*git .{png,svg,jpg,jpeg}": "img/",
+    "src/img/*.{png,svg,jpg,jpeg}": "img/",
     // "src/img/DSC_*": "img/",
     // "src/favicon/*.{png,ico,svg,xml,webmanifest}": "img/favicon",
   },
