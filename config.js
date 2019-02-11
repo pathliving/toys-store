e367b2436a83619ca2d1278c868a3996fe70ef0b
Andrii Hrynenko <pathliving@gmail.com>
2019-02-11 13:36:04 +0200

    Remove styles from config.js and fix font name

--- config.js
+++ config.js
@@ -15,9 +15,9 @@ let config = {
   "addStyleBefore": [
     "src/scss/basic.scss",
     "src/scss/fonts.scss",
-    "src/scss/libs.scss",
     "src/scss/mixins.scss",
-    "src/scss/vars.scss"
+    "src/scss/vars.scss",
+    "src/scss/libs.scss"
   ],
   "addStyleAfter": [
     // "src/scss/print.scss"
@@ -28,10 +28,14 @@ let config = {
   ],
   "addAssets": {
     "src/fonts/OpenSans-Light.ttf": "fonts/",
-    "src/fonts/OpenSans.ttf": "fonts/",
+    "src/fonts/OpenSans-Regular.ttf": "fonts/",
     "src/fonts/OpenSans-Semibold.ttf": "fonts/",
     "src/fonts/OpenSans-Bold.ttf": "fonts/",
+    "src/fonts/VAGRounded-Lt-Regular.ttf": "fonts/",
+    "src/fonts/VAGRounded-Lt-Bold.ttf": "fonts/",
     "src/img/*.{png,svg,jpg,jpeg}": "img/",
+    "node_modules/slick-carousel/slick/slick.min.js" : "js/",
+    "node_modules/jquery/dist/jquery.min.js" : "js/"
     // "src/img/DSC_*": "img/",
     // "src/favicon/*.{png,ico,svg,xml,webmanifest}": "img/favicon",
   },
