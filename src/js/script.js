74db46ee755f67f32b6972465bf145f6a7b25d1c
Andrii Hrynenko <pathliving@gmail.com>
2019-02-11 15:07:01 +0200

    Add interactivity to search block, subnavigation and banner block

--- src/js/script.js
+++ src/js/script.js
@@ -1,7 +1,8 @@
-$(document).ready(function () {
-	svg4everybody({});
+import $ from 'jquery';
 
-	let mainSubnavHover = () => {
+$(document).ready(function () {
+
+	let mainSubnavHover = function () {
 		$('.main-subnav__item').hover(
 			function () {
 				let parentList = $(this).closest('.main-subnav__list');
@@ -14,12 +15,57 @@ $(document).ready(function () {
 				}
 			}, function () {
 				let parentList = $(this).closest('.main-subnav__list');
-				parentList.css('height', 'auto');
 				parentList.css('width', 'auto');
 			}
 		)
 	};
 
+	let openSearchForm = function () {
+		$(document).on('click', '.search__icon', function () {
+			$('.search__body').fadeIn(200);
+		});
+	};
+
+	let clearSearchForm = function () {
+		$(document).on('click', '.search__clear', function () {
+			$('.search__input').val('');
+		});
+	};
+
+	let hideSearchForm = function () {
+		$(document).mouseup(function (e) {
+			var searchForm = $(".search__body");
+			if (searchForm.has(e.target).length === 0){
+				$('.search__body').fadeOut(200);
+			}
+		});
+	};
+
+	let searchButton = function () {
+		$(document).on('click', '.search__btn', function () {
+			alert('Благодарим за ваш интерес!')
+		});
+	};
+
+	let bannerSlider = function () {
+		$('.js-banner').slick({
+			slidesToShow: 1,
+			slidesToScroll: 1,
+			prevArrow: '.banner__navigation--prev',
+			nextArrow: '.banner__navigation--next',
+			dots: true,
+			customPaging: function(slider, i) {
+				return '<a class="banner__dot"></a>';
+			},
+			appendDots: '.banner__dots'
+		})
+	};
+
 	mainSubnavHover();
+	openSearchForm();
+	clearSearchForm();
+	hideSearchForm();
+	searchButton();
+	bannerSlider();
 
 });
