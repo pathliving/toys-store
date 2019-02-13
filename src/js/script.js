38416026e888a1a0ebf8ec9094dcf9a8846f0ea3
Andrii Hrynenko <pathliving@gmail.com>
2019-02-13 21:29:54 +0200

    Add preloader block

--- src/js/script.js
+++ src/js/script.js
@@ -7,7 +7,7 @@ $(document).ready(function () {
 			function () {
 				let parentList = $(this).closest('.main-subnav__list');
 				if ($(this).children('.main-subnav__list').length) {
-					let caNavHeight = $(this).children('.main-subnav__list').outerHeight();
+					let catNavHeight = $(this).children('.main-subnav__list').outerHeight();
 					if (parentList.outerHeight() < catNavHeight) {
 						parentList.css('height', catNavHeight);
 					}
@@ -23,13 +23,13 @@ $(document).ready(function () {
 	let openSearchForm = function () {
 		$(document).on('click', '.search__icon', function () {
 			$('.search__body').fadeIn(200);
-		});
+		})
 	};
 
 	let clearSearchForm = function () {
 		$(document).on('click', '.search__clear', function () {
 			$('.search__input').val('');
-		});
+		})
 	};
 
 	let hideSearchForm = function () {
@@ -38,13 +38,13 @@ $(document).ready(function () {
 			if (searchForm.has(e.target).length === 0){
 				$('.search__body').fadeOut(200);
 			}
-		});
+		})
 	};
 
 	let searchButton = function () {
 		$(document).on('click', '.search__btn', function () {
 			alert('Благодарим за ваш интерес!')
-		});
+		})
 	};
 
 	let bannerSlider = function () {
@@ -61,11 +61,37 @@ $(document).ready(function () {
 		})
 	};
 
+	let tabs = function () {
+		$('.tabs-navigation__item').click(function() {
+			let tabName = $(this).attr('show-tab'),
+					tabsBody = $(this).closest('.tabs').find('.tabs__body')[0],
+					tab = $(tabsBody).find('.' + tabName);
+			$(this).addClass('tabs-navigation__item--active').siblings().removeClass('tabs-navigation__item--active');
+			$(tab).addClass('tab--active').siblings().removeClass('tab--active');
+			if ($(tabsBody).find('.js-products-line-slider').length) {
+				$('.js-products-line-slider').each(function () {
+					$(this).slick('refresh');
+				})
+				$('.js-product-prev__slider').each(function () {
+					$(this).slick('refresh');
+				})
+			}
+		})
+	};
+
 	mainSubnavHover();
 	openSearchForm();
 	clearSearchForm();
 	hideSearchForm();
 	searchButton();
 	bannerSlider();
+	tabs();
 
+});
+
+// Preloader
+$(window).on('load', function () {
+	$('.sk-circle').fadeOut();
+	$('.preloader').delay(400).fadeOut('slow');
+	$('body').removeClass('fixed');
 });
